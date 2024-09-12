9c8f2cca8fd150db1ea2730c949c0f95235c18b2
Kristjan Mark <3hq@pm.me>
2024-09-12 20:26:52 +0200

    some new maps

--- src/test.py
+++ src/test.py
@@ -1,64 +1,68 @@
-# import plotly.express as px
-import json
+import streamlit as st
 import pandas as pd
-# Load your geojson file
-# with open('../data/raw/ch-districts.geojson') as f:
+import numpy as np 
+import plotly.graph_objects as go
+import plotly.express as px
+import os
+import json
 
-#     geojson_data = json.load(f)
-
-# # Create a DataFrame or dictionary with some sample data to map onto the geojson
+# Create a DataFrame or dictionary with some sample data to map onto the geojson
 # data = {
-#     'BEZIRKSNUM': [1901],  # Example district number (must match "BEZIRKSNUM" in GeoJSON)
-#     'Population': [78305]  # Example population data to map
+#     'BEZIRKSNUM': [1901, 1843],  # Example district number (must match "BEZIRKSNUM" in GeoJSON)
+#     'EINWOHNERZ': [78305, 92000]  # Example population data to map
 # }
-
 # df = pd.DataFrame(data)
 
-# # Create the choropleth map using Plotly Express
-# fig = px.choropleth_mapbox(df,
-#                            geojson=geojson_data,
-#                            locations='BEZIRKSNUM',
-#                            featureidkey="properties.BEZIRKSNUM",  # This must match the property in your GeoJSON
-#                            color='Population',
-#                            mapbox_style="carto-positron",
-#                            center={"lat": 47.0, "lon": 8.0},  # Switzerland coordinates
-#                            zoom=7)
+df = pd.read_csv('../data/processed/geojson_df_including_pops.csv', encoding='utf-8')
 
-# fig.update_layout(margin={"r":0,"t":0,"l":0,"b":0})
+with open('../data/raw/ch-districts.geojson') as f:
+    geojson_data = json.load(f)
 
-# fig.show()
+# Create the choropleth map using Plotly Express
+fig = px.choropleth_mapbox(df,
+                           geojson=geojson_data,
+                           locations='BEZIRKSNUM', # this must match featureidkey
+                           featureidkey="properties.BEZIRKSNUM",  # This must match the property in your GeoJSON
+                           color='EINWOHNERZ',
+                           mapbox_style="carto-positron",
+                           center={"lat": 47.0, "lon": 8.0},  # Switzerland coordinates
+                           zoom=7)
+
+fig.update_layout(margin={"r":0,"t":0,"l":0,"b":0})
+
+fig.show()
 
 
 # import json
 
-def extract_bezirksnum_name_mapping(geojson_file):
-    with open(geojson_file, 'r') as file:
-        geojson_data = json.load(file)
+# def extract_bezirksnum_name_mapping(geojson_file):
+#     with open(geojson_file, 'r') as file:
+#         geojson_data = json.load(file)
     
-    bezirksnum_name_map = {}
-    for feature in geojson_data['features']:
-        properties = feature['properties']
-        if 'BEZIRKSNUM' in properties and 'NAME' in properties:
-            bezirksnum = properties['BEZIRKSNUM']
-            name = properties['NAME']
-            bezirksnum_name_map[bezirksnum] = name
+#     bezirksnum_name_map = {}
+#     for feature in geojson_data['features']:
+#         properties = feature['properties']
+#         if 'BEZIRKSNUM' in properties and 'NAME' in properties:
+#             bezirksnum = properties['BEZIRKSNUM']
+#             name = properties['NAME']
+#             bezirksnum_name_map[bezirksnum] = name
     
-    return bezirksnum_name_map
+#     return bezirksnum_name_map
 
-# Usage
-bezirksnum_name_mapping = extract_bezirksnum_name_mapping('../data/raw/ch-districts.geojson')
-print(bezirksnum_name_mapping)
+# # Usage
+# bezirksnum_name_mapping = extract_bezirksnum_name_mapping('../data/raw/ch-districts.geojson')
+# print(bezirksnum_name_mapping)
 
 
-def save_bezirksnum_name_to_csv(geojson_file, output_csv_file):
-    # Extract the mapping
-    bezirksnum_name_mapping = extract_bezirksnum_name_mapping(geojson_file)
+# def save_bezirksnum_name_to_csv(geojson_file, output_csv_file):
+#     # Extract the mapping
+#     bezirksnum_name_mapping = extract_bezirksnum_name_mapping(geojson_file)
     
-    # Convert to DataFrame
-    df = pd.DataFrame(list(bezirksnum_name_mapping.items()), columns=['BEZIRKSNUM', 'NAME'])
+#     # Convert to DataFrame
+#     df = pd.DataFrame(list(bezirksnum_name_mapping.items()), columns=['BEZIRKSNUM', 'NAME'])
     
-    # Save to CSV
-    df.to_csv(output_csv_file, encoding='utf-8')
+#     # Save to CSV
+#     df.to_csv(output_csv_file, encoding='utf-8')
 
-# Usage
-save_bezirksnum_name_to_csv('../data/raw/ch-districts.geojson', 'bezirksnum_name_mapping.csv')
+# # Usage
+# save_bezirksnum_name_to_csv('../data/raw/ch-districts.geojson', 'bezirksnum_name_mapping.csv')
