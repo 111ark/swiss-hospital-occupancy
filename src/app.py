0ba763de8c04915f2be9ac7e18f98445aae4a5cc
Kristjan Mark <3hq@pm.me>
2024-09-12 15:42:25 +0200

    cleaned

--- src/app.py
+++ src/app.py
@@ -6,6 +6,43 @@ import plotly.express as px
 import os
 import json
 
+pop_df = pd.read_csv('../data/processed/district_pop.csv')
+# district geodata
+
+with open('../data/raw/ch-districts.geojson') as f:
+	geodata = json.load(f)
+
+
+
+fig = px.choropleth_mapbox(
+    pop_df,
+    geojson=geodata,
+    color='pop23',
+    locations='name',
+    featureidkey="properties.NAME",
+    color_continuous_scale=px.colors.sequential.algae,
+    )
+
+fig.update_layout(mapbox_style="carto-positron",
+                  mapbox_zoom=6, mapbox_center = {"lon": 8.2328637, "lat": 46.7995666})
+fig.update_layout(margin={"r":0,"t":0,"l":0,"b":0})
+st.plotly_chart(fig)
+
+
+
+
+
+
+# @st.cache_data
+# def load_json(path:str) -> object:
+#     with open(path) as f:
+#         geodata = json.load(f)
+#     return geodata
+
+# geodata_raw = load_json('./data/raw/georef-switzerland-kanton.geojson')
+# geodata = deepcopy(geodata_raw)
+
+
 # # Load the dataset
 # df_path = os.path.abspath("../data/processed/address_coordinates.csv")
 # df = pd.read_csv(df_path)
@@ -19,39 +56,13 @@ import json
 # df = pd.read_csv(pop_path)
 
 
-pop_df = pd.read_csv('../data/processed/district_pop.csv')
-# district geodata
-
-with open('../data/raw/ch-districts.geojson') as f:
-	geodata = json.load(f)
-
-
-
 # population = powerplants.groupby(by=['canton', 'energy_source_level_2'], as_index=False) \
 #     .agg({'electrical_capacity':'sum'}) \
 #     .pivot(index='canton', columns='energy_source_level_2', values='electrical_capacity') \
 #     .fillna(0) 
 
-fig = px.choropleth_mapbox(
-    pop_df,
-    geojson=geodata,
-    color='pop23',
-    locations='pop23',
-    featureidkey="properties.kan_name",
-    color_continuous_scale=px.colors.sequential.algae,
-    )
 
-fig.update_layout(mapbox_style="carto-positron",
-                  mapbox_zoom=6, mapbox_center = {"lon": 8.2328637, "lat": 46.7995666})
-fig.update_layout(margin={"r":0,"t":0,"l":0,"b":0})
-st.plotly_chart(fig)
+# healcare cost is on rise.. we visualize how well used facilities in CH are, by giving an overview
+# and simulating  current or smaller number of hospitals to see how the demamd will distribute.
 
-@st.cache_data
-def load_json(path:str) -> object:
-    with open(path) as f:
-        geodata = json.load(f)
-    return geodata
-
-geodata_raw = load_json('./data/raw/georef-switzerland-kanton.geojson')
-geodata = deepcopy(geodata_raw)
 
